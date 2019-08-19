06ce9349b8ffa23fe772c5f04b56111d777ee728
xiaoh <2924127649@qq.com>
2019-08-19 15:22:00 +0800

    Initial commit

---
+++ src/main/java/com/bx/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.bx.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiaoh
+ * @create 2019-08-19-14:42
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String hello(){
+        return "hello+tom";
+    }
+
+}
