109ebc217e4565f8b54b661f56a73c545dce3fc9
xiaoh <2924127649@qq.com>
2019-08-20 16:26:42 +0800

    11

--- src/main/java/com/bx/controller/HelloController.java
+++ src/main/java/com/bx/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String hello(){
-        return "hello+tangsheng111";
+        return "hello+niumowang";
     }
 
 }
